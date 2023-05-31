b05143b9798a22f5baf4bf55abcf26b69d90e285
qlentz <53540127+qlentz@users.noreply.github.com>
2023-06-01 00:15:29 +0200

    s

--- php_cgi.php
+++ php_cgi.php
@@ -1,14 +1,14 @@
 <?php
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        header("Location: " . $_SERVER['PHP_SELF']);
         $input = fgets(STDIN);
 
         parse_str($input, $output);
-
         if (isset($output['fname']) && isset($output['lname'])) {
             $fname = $output['fname'];
             $lname = $output['lname'];
 
-            $file = 'user_data.txt';
+            $file = './www/user_data.txt';
             $current = file_get_contents($file);
 
             // Append new data to the file
@@ -16,18 +16,34 @@
 
             // Write contents back to the file
             file_put_contents($file, $current);
-			header("Location: php_cgi.php", true, 303);
-			exit();
+            echo '<script type="text/javascript">
+            window.location.reload(true);
+       </script>';
+            exit();
         }
     } elseif ($_SERVER['REQUEST_METHOD'] == 'GET') {
-        $file = 'user_data.txt';
+        $file = './www/user_data.txt';
 
         // If the file exists, display its contents
         if (file_exists($file)) {
 			$contents = file_get_contents($file);
             echo "<html>\n<body>\n<h1>User Data:</h1>\n<p>";
             echo nl2br($contents);
+            echo '<button onclick="window.location.href=\'index.html\';">Go Back</button>';
             echo "</p>\n</body>\n</html>";
+        }
+    } elseif ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
+        $file = './www/user_data.txt';
+        if (file_exists($file)) {
+            $lines = file($file);
+            array_pop($lines); // remove the last line
+            file_put_contents($file, $lines);
+
+            // Redirect back to the script
+            echo '<script type="text/javascript">
+            window.location.reload(true);
+       </script>';
+            exit();
         }
     }
 ?>
